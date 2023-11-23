d81386c113439231350358384ebc5098279d3c7c
qwq <ikitaqwq@ya.com>
2023-11-23 16:34:31 +0500

    1

---
+++ 1.py
@@ -0,0 +1,17 @@
+import io
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.cu_dr)
+
+    def paintEvent(self):
+
+    def
+
